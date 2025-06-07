98095d3658a1846766e6d51cf1bdc901d9ac80b2
levi goossens <Levi.Goossens@STUDENT.PXL.BE>
2025-06-07 17:56:55 +0200

    draw numbers of sudoku

--- script1.py
+++ script1.py
@@ -11,6 +11,15 @@ def draw_visual_board():
         pg.draw.line(surface, "black", ((40*i)+1.5, 0), ((40*i)+1.5, (40*9)+2), width)
     return surface
 
+def draw_numbers(surf):
+    font = pg.font.SysFont("ubuntu", 40)
+    for i in range(9):
+        for j in range(9):
+            font_surface = font.render("9", True, "black")
+            font_rect = font_surface.get_rect(center=((40*i)+20, (40*j)+20))
+            surf.blit(font_surface, font_rect)
+    return surf
+
 pg.init()
 
 screen = pg.display.set_mode((1280*1.5,720*1.5))
@@ -21,7 +30,9 @@ while running:
         if event.type == pg.QUIT:
             running = False
     screen.fill("white")
-    screen.blit(draw_visual_board(), (50, 50))
+    surf1 = draw_visual_board()
+    draw_numbers(surf1)
+    screen.blit(surf1, (50, 50))
     pg.display.flip()
 
 pg.quit()
