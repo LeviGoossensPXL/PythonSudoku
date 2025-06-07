d03127057167f646091d58fe64d70b1398e62d80
levi goossens <Levi.Goossens@STUDENT.PXL.BE>
2025-06-07 17:21:43 +0200

    draw sudoku board

--- script1.py
+++ script1.py
@@ -1,12 +1,15 @@
 import pygame as pg
 
-def draw_visual_board(surface):
-    for i in range(1, 11):
+def draw_visual_board():
+    surface = pg.Surface(((40*9)+3, (40*9)+3))
+    surface.fill("white")
+    for i in range(10):
         width = 1
-        if (i-1) % 3 == 0:
+        if i % 3 == 0:
             width = 3
-        pg.draw.line(surface, "black", (40, 40*i), (40+(40*9), 40*i), width)
-        pg.draw.line(surface, "black", (40*i, 40), (40*i, 40+(40*9)), width)
+        pg.draw.line(surface, "black", (0, (40*i)+1.5), ((40*9)+2, (40*i)+1.5), width)
+        pg.draw.line(surface, "black", ((40*i)+1.5, 0), ((40*i)+1.5, (40*9)+2), width)
+    return surface
 
 pg.init()
 
@@ -18,9 +21,7 @@ while running:
         if event.type == pg.QUIT:
             running = False
     screen.fill("white")
-    draw_visual_board(screen)
+    screen.blit(draw_visual_board(), (50, 50))
     pg.display.flip()
 
 pg.quit()
-
-
